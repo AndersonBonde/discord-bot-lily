4475a7c7eec2b3f098e3c3f6d72ed4772270be55
Anderson Bonde <andersonbonde@gmail.com>
2024-08-08 17:40:18 -0300

    Listen on port 3000

--- index.js
+++ index.js
@@ -35,3 +35,17 @@ for (const file of eventFiles) {
 }
 
 client.login(process.env.TOKEN);
+
+import express from 'express';
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+	res.json({
+		message: 'Hello, world!',
+	});
+});
+
+app.listen(port, () => {
+	console.log(`App is listening on port ${port}`);
+});
