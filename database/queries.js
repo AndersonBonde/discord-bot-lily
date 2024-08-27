853380846dcb796f609fe6de1f60f45756d5b6ae
Anderson Bonde <andersonbonde@gmail.com>
2024-08-27 13:45:27 -0300

    Add getAllPowers query

--- database/queries.js
+++ database/queries.js
@@ -1,11 +1,18 @@
 import { pool } from './pool.js';
 
+async function getAllPowers() {
+	const { rows } = await pool.query('SELECT * FROM powers');
+
+	return rows;
+};
+
 async function getPowerByName(name) {
 	const { rows } = await pool.query('SELECT * FROM power WHERE name = $1', [name]);
 
 	return rows[0];
-}
+};
 
 export {
+	getAllPowers,
 	getPowerByName,
 };
